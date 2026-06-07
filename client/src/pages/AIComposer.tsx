e3e55e0934e9463449c34b5bfc3eb014c99c4116
Naman Bharsakale <namanbharsakale045@gmail.com>
2026-06-07 15:49:13 +0530

    feat: added layout and side bar

---
+++ client/src/pages/AIComposer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AIComposer() {
+  return (
+    <div>AIComposer</div>
+  )
+}
+
+export default AIComposer
---
+++ client/src/pages/Schedular.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Schedular() {
+  return (
+    <div>Schedular</div>
+  )
+}
+
+export default Schedular
