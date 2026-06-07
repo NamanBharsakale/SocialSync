61d5d1eb2a3bcfcb76dc0283af346b50fc874f17
Naman Bharsakale <namanbharsakale045@gmail.com>
2026-06-07 21:48:54 +0530

    ui: designed dashboard uiux

--- client/src/pages/Accounts.tsx
+++ client/src/pages/Accounts.tsx
@@ -1,6 +1,25 @@
+import { useState } from "react"
+
 function Accounts() {
+
+    const [accounts,setAccounts] = useState
   return (
-    <div>Accounts</div>
+    <div className="space-y-8 max-w-4xl ">
+        {/**Header */}
+        <div>
+            <div>
+                <h2>Connected Accounts</h2>
+                <p></p>
+            </div>
+            <button></button>
+        </div>
+
+        {/*Platform picker model*/}
+
+
+        {/*Connected accounts list */}
+
+    </div>
   )
 }
 
