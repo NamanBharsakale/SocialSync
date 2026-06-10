230924b462ec628ca0855744cb8500c7638f6b7a
Naman Bharsakale <namanbharsakale045@gmail.com>
2026-06-10 23:03:17 +0530

    tests

---
+++ server/server.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from 'express';
+import cors from "cors";
+import connectDB from "./config/db";
+
+const app = express();
+
+//db connection
+await connectDB()
+
+//global error handler
+app.use((err:any,_req:Request,res:Response,_next: NextFunction)=>{
+    console.error(err)
+    res.status(500).send(err?.response?.data?.message)
+})
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
