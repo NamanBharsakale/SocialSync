e1d18f018c73f09241d6a7c9a2cffbb2cc1c03e3
Naman Bharsakale <namanbharsakale045@gmail.com>
2026-06-11 09:48:47 +0530

    declared authROuter

---
+++ server/routes/authRouter.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { loginUser, registerUser } from "../controllers/authController.js";
+
+
+const authRouter = Router();
+
+authRouter.post('/register',registerUser)
+
+authRouter.post('/login',loginUser)
+
+
+export default authRouter;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
