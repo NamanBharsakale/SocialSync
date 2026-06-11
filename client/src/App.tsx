7181a4616afca5baf3d1bab2a71c086a90aa299b
Naman Bharsakale <namanbharsakale045@gmail.com>
2026-06-12 00:42:41 +0530

    wired login feature

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,10 +6,12 @@ import Dashboard from "./pages/Dashboard";
 import Schedular from "./pages/Schedular";
 import AIComposer from "./pages/AIComposer";
 import Accounts from "./pages/Accounts";
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
     return (
         <>
+            <Toaster position="top-right"/>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
