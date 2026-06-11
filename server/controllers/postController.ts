76b38f553d1facd85a2ff31fe132f2cf21d476e3
Naman Bharsakale <namanbharsakale045@gmail.com>
2026-06-11 21:50:46 +0530

    init: multer setup added

--- server/controllers/postController.ts
+++ server/controllers/postController.ts
@@ -4,6 +4,8 @@ import { GoogleGenAI } from "@google/genai";
 import axios from "axios";
 import { cloudinary } from "../config/cloudinary.js";
 import { Generation } from "../model/Generation.js";
+import { resolve } from "node:dns";
+import { Post } from "../model/Posts.js";
 
 //Helper to poll leonardo.ai
 const pollLeonardoJob = async (generationId: string,apiKey:string):Promise<string>=>{
@@ -150,18 +152,82 @@ export const generatePost = async (req:AuthRequest,res:Response):Promise<void>=>
 //Get generatiosn
 //GET /api/posts/generations
 export const getGenerations = async (req:AuthRequest,res:Response):Promise<void>=>{
-    
+    try {
+        const generations = await Generation.find({
+            user:req.user._id
+        }).sort({createdAt:-1})
+        
+        res.json(generations)
+    } catch (error:any) {
+        res.status(500).json({
+            message: error?.message || "Server Error"
+        })
+    }
 }
 
 //Get post
 //GET /api/posts
 export const getPosts = async (req:AuthRequest,res:Response):Promise<void>=>{
-
+    try {
+        const posts = await Post.find({user: req.user_id})
+        res.json(posts)
+    } catch (error) {
+        res.status(500).json({
+            message: error?.message || "Server Error"
+        })
+    }
 }
 
 
 //Schedule post
 //GET /api/posts
 export const schedulePost = async (req:AuthRequest,res:Response):Promise<void>=>{
+    try {
+        const {content,platforms,scheduledFor,status} = req.body;
 
+        //parse platforms if it comes as a stringified array fromo FormData
+        let parsedPlatforms = platforms;
+
+        if(typeof platforms === "string"){
+            try {
+                parsedPlatforms = JSON.parse(platforms)
+            } catch (error) {
+                parsedPlatforms = platforms.split(",")
+            }
+        }
+        let mediaUrl:string  | undefined = req.body.mediaUrl;
+        let mediaType: "image" | "video" | undefined = req.body.mediaType;
+
+        if(req.file){
+            const result = await new Promise<any>((resolve,reject)=>{
+                const stream = cloudinary.uploader.upload_stream({
+                    resource_type:"auto",
+                    folder:"social-sync"
+                },(error,result)=>{
+                    if(error) reject(error);
+                    else resolve(result)
+                });
+                stream.end(req.file!.buffer);
+            });
+            mediaUrl = result.secure_url;
+            mediaType = result.resource_type === "video" ? "video":"image";
+
+        }
+        const post = await Post.create({
+            user: req.user._id,
+            content,
+            platforms: parsedPlatforms,
+            mediaUrl,
+            mediaType,
+            scheduledFor,
+            status
+        })
+
+        res.status(201).json(post)
+
+    } catch (error) {
+        res.status(500).json({
+            message: error?.message || "Server Error"
+        })
+    }
 }
